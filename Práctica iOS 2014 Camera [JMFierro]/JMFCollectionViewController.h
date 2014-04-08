78912657ccbe79b120b946453797e92e1c378107
José Manuel Fierro Conchouso <jose.fierro@live.u-tad.com>
2014-04-08 16:15:46 +0200

    Collection & Flickr busqueda

---
+++ Práctica iOS 2014 Camera [JMFierro]/JMFCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMFCollectionViewController.h
+//  Práctica iOS 2014 Camera [JMFierro]
+//
+//  Created by José Manuel Fierro Conchouso on 05/04/14.
+//  Copyright (c) 2014 José Manuel Fierro Conchouso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMFCollectionViewController : UIViewController
+
+@end
---
+++ Práctica iOS 2014 Camera [JMFierro]/JMFCollectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  JMFCollectionViewController.m
+//  Práctica iOS 2014 Camera [JMFierro]
+//
+//  Created by José Manuel Fierro Conchouso on 05/04/14.
+//  Copyright (c) 2014 José Manuel Fierro Conchouso. All rights reserved.
+//
+
+#import "JMFCollectionViewController.h"
+
+@interface JMFCollectionViewController ()
+
+@end
+
+@implementation JMFCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
